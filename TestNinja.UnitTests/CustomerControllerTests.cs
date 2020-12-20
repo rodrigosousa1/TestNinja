1cb7d8874028daaf5b8b3e773a29c8b40421bb9a
Rodrigo Oliveira <rdgsec@gmail.com>
2020-12-20 16:33:27 -0300

    initial commit

---
+++ TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+        private CustomerController _controller;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _controller = new CustomerController();
+        }
+
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            var result = _controller.GetCustomer(0);
+
+            // NotFound
+            Assert.That(result, Is.TypeOf<NotFound>());
+
+            // NotFound or one of its derivatives
+            //Assert.That(result, Is.InstanceOf<NotFound>());
+        }
+        [Test]
+        public void GetCustomer_IdIsNotZero_ReturnOk()
+        {
+            var result = _controller.GetCustomer(1);
+
+            Assert.That(result, Is.TypeOf<Ok>());
+        }
+    }
+}
---
+++ TestNinja.UnitTests/ReservationTests.cs
@@ -0,0 +1,47 @@
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class ReservationTests
+    {
+        [Test]
+        public void CanBeCancelledBy_AdminCancelling_ReturnsTrue()
+        {
+            // Arrange
+            var reservation = new Reservation();
+
+            // Act
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            // Assert
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void CanBeCancelledBy_SameUserCancellingTheReservation_ReturnsTrue()
+        {
+            // Arrange
+            var user = new User();
+            var reservation = new Reservation { MadeBy = user };
+            // Act
+            var result = reservation.CanBeCancelledBy(user);
+
+            // Assert
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void CanBeCancelledBy_AnotherUserCancellingTheReservation_ReturnsFalse()
+        {
+            // Arrange
+            var reservation = new Reservation() { MadeBy = new User() };
+            // Act
+            var result = reservation.CanBeCancelledBy(new User());
+
+            // Assert
+            Assert.That(result, Is.False);
+        }
+    }
+}
